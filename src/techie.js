8131dfcf4ac841713fc7d6e2c9131a774a390b40
unknown <sadiiqduceysane@gmail.com>
2020-04-18 12:31:07 +0530

    made changes to views and partials and also techie.js

--- src/techie.js
+++ src/techie.js
@@ -1,14 +1,25 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+
 const app = express();
 const port = process.env.PORT || 3000;
-// using path.join and dirname for  files in public folder
-app.use(express.static(path.join(__dirname, '../public')))
+
+// Defining the paths
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// setting up handlerbars and location of views
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
 // Registering partials
-hbs.registerPartials(__dirname+ '../../views/partials')
-// setting the view engine
-app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname+ '../views/partials')
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear()
